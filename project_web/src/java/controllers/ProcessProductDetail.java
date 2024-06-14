2f847a4d68bffcbbb62cad835443908eaff676de
PhucAnh0502 <phucanh05022004@gmail.com>
2024-06-14 21:50:29 +0700

    learning MVC

---
+++ project_web/src/java/controllers/ProcessProductDetail.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controllers;
+
+import DAO.ProductDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Product;
+
+/**
+ *
+ * @author PHUCANH
+ */
+@WebServlet(name = "ProcessProductDetail", urlPatterns = {"/product-detail"})
+public class ProcessProductDetail extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String id = request.getParameter("id");
+        Product product = ProductDAO.getProductById(Integer.parseInt(id));
+        if(product != null){
+            request.setAttribute("product", product);
+        } else {
+            request.setAttribute("message", "Cannot find item");
+        }
+        RequestDispatcher dis = request.getRequestDispatcher("product-detail.jsp");
+        dis.forward(request, response);
+    }
+
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
